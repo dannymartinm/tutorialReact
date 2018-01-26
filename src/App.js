29a8b91043903024eb328a42a987322fed46a5fd
Daniela Martin <danielamartinmtz@gmail.com>
2018-01-26 13:52:59 -0600

    Video 14. Obtener datos de un API y filtrado

--- src/App.js
+++ src/App.js
@@ -212,7 +212,7 @@ class Wrapper extends Component {
 export default Wrapper;
 
 */
-
+/*
 class App extends Component {
   constructor() {
     super();
@@ -247,4 +247,37 @@ class App extends Component {
 }
 
 App.defaultProps = { val: 0 };
+
+*/
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { items: [] };
+  }
+  componentWillMount() {
+    fetch("https://www.swapi.co/api/people/?format=json")
+      .then(response => response.json())
+      .then(({ results: items }) => this.setState({ items }));
+  }
+
+  filter(e) {
+    this.setState({ filter: e.target.value });
+  }
+  render() {
+    let items = this.state.items;
+    if (this.state.filter) {
+      items = items.filter(item =>
+        item.name.toLowerCase().includes(this.state.filter.toLowerCase())
+      );
+    }
+    return (
+      <div>
+        <input type="text" onChange={this.filter.bind(this)} />
+        {items.map(item => <Person key={item.name} person={item} />)}
+      </div>
+    );
+  }
+}
+
+const Person = props => <h4>{props.person.name}</h4>;
 export default App;
