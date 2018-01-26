a7303de4d44b91d5863123859218010cb2da557e
Daniela Martin <danielamartinmtz@gmail.com>
2018-01-26 14:36:59 -0600

    Video 15. Compose React Component Behavior with Higher Order Components

--- src/App.js
+++ src/App.js
@@ -248,7 +248,9 @@ class App extends Component {
 
 App.defaultProps = { val: 0 };
 
-*/
+Get Data from an API */
+
+/*
 class App extends Component {
   constructor() {
     super();
@@ -280,4 +282,67 @@ class App extends Component {
 }
 
 const Person = props => <h4>{props.person.name}</h4>;
+*/
+
+/*
+//Higher Order Components
+const HOC = InnerComponent =>
+  class extends Component {
+    constructor() {
+      super();
+      this.state = { count: 0 };
+    }
+
+    update() {
+      this.setState({ count: this.state.count + 1 });
+    }
+
+    componentWillMount() {
+      console.log("will mount");
+    }
+
+    render() {
+      return (
+        <InnerComponent
+          {...this.props}
+          {...this.state}
+          update={this.update.bind(this)}
+        />
+      );
+    }
+  };
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>Button</Button>
+        <hr />
+        <LabelHOC>Label</LabelHOC>
+      </div>
+    );
+  }
+}
+
+const Button = HOC(props => (
+  <button onClick={props.update}>
+    {props.children} - {props.count}
+  </button>
+));
+
+class Label extends Component {
+  componentWillMount() {
+    console.log("label will mount");
+  }
+
+  render() {
+    return (
+      <label onMouseMove={this.props.update}>
+        {this.props.children}- {this.props.count}
+      </label>
+    );
+  }
+}
+const LabelHOC = HOC(Label);
+*/
+
 export default App;
