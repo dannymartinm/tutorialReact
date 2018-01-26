76c540a78ec8482de54b79fc860124891672c928
Daniela Martin <danielamartinmtz@gmail.com>
2018-01-26 16:52:38 -0600

    Video 19. Botones con selected item

--- src/App.js
+++ src/App.js
@@ -345,6 +345,8 @@ class Label extends Component {
 const LabelHOC = HOC(Label);
 */
 
+//video 19
+/*
 class App extends Component {
   render() {
     return (
@@ -365,6 +367,42 @@ class Parent extends Component {
     //let items = React.Children.toArray(this.props.children);
     console.log(items);
     return null;
+  }
+}
+*/
+
+class App extends Component {
+  render() {
+    return (
+      <Buttons>
+        <button value="A">A</button>
+        <button value="B">B</button>
+        <button value="C">C</button>
+      </Buttons>
+    );
+  }
+}
+
+class Buttons extends Component {
+  constructor() {
+    super();
+    this.state = { selected: "None" };
+  }
+  selectItem(selected) {
+    this.setState({ selected });
+  }
+  render() {
+    let fn = child =>
+      React.cloneElement(child, {
+        onClick: this.selectItem.bind(this, child.props.value)
+      });
+    let items = React.Children.map(this.props.children, fn);
+    return (
+      <div>
+        <h2>You have Selected: {this.state.selected}</h2>
+        {items}
+      </div>
+    );
   }
 }
 export default App;
